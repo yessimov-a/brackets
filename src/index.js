2906b5786f19733f8a4dedc1f0f6ab6e95d56f00
yessimov-a <a.yessimov98@gmail.com>
2019-10-08 00:50:27 +0600

    finish brackets project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0;
+  let reg = [];
+  while (i < bracketsConfig.length) {
+    let regStr = Number.isInteger(+bracketsConfig[i][0]) ? `${bracketsConfig[i][0]}${bracketsConfig[i][1]}` : `\\${bracketsConfig[i][0]}\\${bracketsConfig[i][1]}`;
+    reg.push(regStr);
+    i++;
+  }
+  const regExp = new RegExp(reg.join('|'), 'g');
+  while (regExp.test(str)) {
+    str = str.replace(regExp, '')
+  }
+  return str.length === 0;
 }
+
+
+
